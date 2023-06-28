22f8d9b2bd355eefcdc6544aad03940b361e6423
davodo88 <92156631+davodo88@users.noreply.github.com>
2023-06-28 12:58:37 +0200

    image about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 
-import David from '../../images/David.jpg'
+import David from '../../images/yo3.jpg'
 import SkillsBox from '../components/atoms/skillsBox'
 
 
